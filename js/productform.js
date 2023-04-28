7f7a786bfbb94e9fa7279d1abb4cf3fb1bfcafed
erlm <erlm@kea.dk>
2023-04-28 09:45:03 +0200

    create product virker

--- js/productform.js
+++ js/productform.js
@@ -13,6 +13,42 @@ async function handleFormSubmit(event) {
     event.preventDefault();
     const formData = new FormData(formProduct);
     console.log(formData);
+    const jsonToPost = convertFormDataToJson(formData)
+    console.log(jsonToPost)
+    try {
+        const responseData = await postFormDataAsJson(urlPostProduct, jsonToPost);
+        console.log(responseData)
+        //her kan man indsæt nyt product i tabellen
+    } catch (error) {
+        alert(error.message);
+        console.error(error);
+    }
 }
+
+function convertFormDataToJson(formData) {
+    // laver formData til JSON
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJsonString = JSON.stringify(plainFormData);
+    console.log(formDataJsonString);
+    return formDataJsonString
+}
+
+async function postFormDataAsJson(url, jsonToSend) {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: jsonToSend,
+    };
+    const response = await fetch(url, fetchOptions);
+    if (!response.ok) {
+        const errorMessage = await response.text();
+        throw new Error(errorMessage);
+    }
+    return response.json();
+}
+
+
 
 
